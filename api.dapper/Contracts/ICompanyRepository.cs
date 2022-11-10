3e5d3145062afbe86f389edc32d1521bf2036ae8
Tùng <jeniferchoe2712@gmail.com>
2022-11-10 02:37:19 -0800

    Add files via upload

---
+++ api.dapper/Contracts/ICompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using api.dapper.Dto;
+using api.dapper.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.dapper.Contracts
+{
+    public interface ICompanyRepository
+    {
+        Task<IEnumerable<Company>> GetCompanies();
+        Task<Company> GetCompany(int id);
+        Task<Company> CreateCompany(CompanyForCreationDto company);
+        Task UpdateCompany(int id, CompanyForUpdateDto company);
+        Task DeleteCompany(int id);
+        Task<Company> GetCompanyByEmployeeId(int id);
+        Task<Company> GetCompanyEmployeesMultipleResults(int id);
+        Task<List<Company>> GetCompaniesEmployeesMultipleMapping();
+    }
+}
